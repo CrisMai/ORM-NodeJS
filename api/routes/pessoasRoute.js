9bfd9747ed12a76936005004789a7d0ccb4b4b31
unknown <crismai.dev@gmail.com>
2023-04-17 14:11:22 -0300

    Métodos de CRUD - buscando uma pessoa

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,6 +4,7 @@ const PessoaController = require('../controllers/PessoaController');
 const router = Router();
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas);
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
 
 
 module.exports = router;
