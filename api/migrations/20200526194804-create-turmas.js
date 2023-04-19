251216b41db7d26c79eb60d6bcd9ebcd7e367f6e
unknown <crismai.dev@gmail.com>
2023-04-19 14:56:17 -0300

    Referenciando tabelas

--- api/migrations/20200526194804-create-turmas.js
+++ api/migrations/20200526194804-create-turmas.js
@@ -8,30 +8,38 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+
       docente_id: {
         allowNull: false,
         type: Sequelize.INTEGER,
         references: { model: 'Pessoas', key: 'id'}
       },
+
       data_inicio: {
         type: Sequelize.DATEONLY
       },
+
       nivel_id: {
         allowNull: false,
         type: Sequelize.INTEGER,
         references: { model: 'Niveis', key: 'id'}
       },
+
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
       },
+
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
       }
     });
+
   },
+
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('Turmas');
   }
+  
 };
--- api/models/matriculas.js
+++ api/models/matriculas.js
@@ -3,14 +3,18 @@ module.exports = (sequelize, DataTypes) => {
   const Matriculas = sequelize.define('Matriculas', {
     status: DataTypes.STRING
   }, {});
+
   Matriculas.associate = function(models) {
     Matriculas.belongsTo(models.Pessoas, {
       foreignKey: 'estudante_id'
     })
+
     Matriculas.belongsTo(models.Turmas, {
       foreignKey: 'turma_id'
     })
 
   };
+
   return Matriculas;
+  
 };
