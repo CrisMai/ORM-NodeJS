ec03c89fa3528d6b541ce478d521869e102b92ef
unknown <crismai.dev@gmail.com>
2023-04-22 16:25:40 -0300

    Restaurando os demais registros

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -11,6 +11,10 @@ router.delete('/pessoas/:id', PessoaController.apagaPessoa);
 router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula);
 router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula);
 router.post('/pessoas/:id/restaura', PessoaController.restauraPessoa);
+router.post('/pessoas/:id/restaura', PessoaController.restauraPessoa);
+router.post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula);
+router.post('/niveis/:id/restaura', NivelController.restauraNivel);
+router.post('/turmas/:id/restaura', TurmaController.restauraTurma);
 router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula);
 router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula);
 
