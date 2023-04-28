3deed17b6744d99b2c231fae6c6453b5ef28886c
unknown <crismai.dev@gmail.com>
2023-04-28 13:50:02 -0300

    Adicionando serviços

---
+++ api/services/index.js
@@ -0,0 +1,12 @@
+const pessoasServices = require('./PessoasServices');
+const turmasServices = require('./TurmasServices');
+const niveisServices = require('./NiveisServices');
+
+
+
+module.exports = {
+    PessoasServices: PessoasServices,
+    TurmasServices: TurmasServices,
+    NiveisServices: NiveisServices
+
+}
