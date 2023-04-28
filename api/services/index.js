ec680a905c7d0148fac57b377fc5d44d61fe9657
unknown <crismai.dev@gmail.com>
2023-04-28 14:25:46 -0300

    Passando parâmetros

--- api/services/index.js
+++ api/services/index.js
@@ -1,12 +1,14 @@
 const pessoasServices = require('./PessoasServices');
 const turmasServices = require('./TurmasServices');
-const niveisServices = require('./NiveisServices');
+const NiveisServices = require('./NiveisServices');
+const MatriculasServices = require('./MatriculasServices');
 
 
 
 module.exports = {
     PessoasServices: PessoasServices,
     TurmasServices: TurmasServices,
-    NiveisServices: NiveisServices
+    NiveisServices: NiveisServices,
+    MatriculasServices: MatriculasServices
 
 }
